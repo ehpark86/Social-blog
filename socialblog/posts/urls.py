d0f52ae091e886ce8cca7e45819f706884114326
Eugene park <ehpark86@gmail.com>
2019-04-24 13:59:52 -0700

    modified urls for posts and added templates for posts

--- socialblog/posts/urls.py
+++ socialblog/posts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.PostList.as_view(), name="all"),
     path('new/', views.CreatePost.as_view(), name="create"),
     path('by/<username:username>', views.UserPosts.as_view(), name="for_user"),
-    path('by/<username:username>/<pk:id>', views.PostDetail.as_view(), name="single"),
-    path('delete/<pk:id>', views.DeletePost.as_view(), name="delete"),
+    path('by/<username:username>/<int: pk>', views.PostDetail.as_view(), name="single"),
+    path('delete/<int: pk>', views.DeletePost.as_view(), name="delete"),
 ]
