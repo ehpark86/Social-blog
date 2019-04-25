c934e78dd588f10ef7760192c430c144e4d9b81b
Eugene park <ehpark86@gmail.com>
2019-04-25 16:01:56 -0700

    added join and leave views for groups app and added urls to match

--- socialblog/groups/views.py
+++ socialblog/groups/views.py
@@ -1,17 +1,73 @@
 from django.shortcuts import render
 
+from django.contrib import messages
+
 from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
 from django.urls import reverse
 from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.db import IntegrityError
 from .models import Group, GroupMember
-# Create your views here.
+from . import models
+
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
     fields = ('name', 'description')
     model = Group
 
+
 class SingleGroup(generic.DeleteView):
     model = Group
 
+
 class ListGroups(generic.ListView):
     model = Group
+
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        # once you are a member, go back to the single detail page
+        return reverse("groups:single", kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group,slug=self.kwargs.get("slug"))
+
+        try:
+            GroupMember.objects.create(user=self.request.user, group=group)
+
+        except IntegrityError:
+            messages.warning(self.request, f"Warning, already a member of {group.name}")
+
+        else:
+            messages.success(self.request, f"You are now a member of the {group.name} group.")
+
+        return super().get(request, *args, **kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single", kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+        # make sure they can't leave a group if they are not part of a group
+            membership = models.GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get("slug")
+            ).get()
+
+        except models.GroupMember.DoesNotExist:
+            messages.warning(
+                self.request,
+                "You can't leave this group that you are not apart of."
+            )
+        else:
+            membership.delete()
+            messages.success(
+                self.request,
+                "You have successfully left this group."
+            )
+        return super().get(request, *args, **kwargs)
