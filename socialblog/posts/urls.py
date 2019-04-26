715aaa41c81eba3c6149210c0b86b73fe21ee227
Eugene park <ehpark86@gmail.com>
2019-04-26 13:37:51 -0700

    changed path to regex

--- socialblog/posts/urls.py
+++ socialblog/posts/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path, include
+from django.urls import path, re_path
 from . import views
 
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name="all"),
-    path('new/', views.CreatePost.as_view(), name="create"),
-    path('by/<username:username>', views.UserPosts.as_view(), name="for_user"),
-    path('by/<username:username>/<int: pk>', views.PostDetail.as_view(), name="single"),
-    path('delete/<int: pk>', views.DeletePost.as_view(), name="delete"),
+    re_path(r"^$", views.PostList.as_view(), name="all"),
+    re_path(r"new/$", views.CreatePost.as_view(), name="create"),
+    re_path(r"by/(?P<username>[-\w]+)/$", views.UserPosts.as_view(), name="for_user"),
+    re_path(r"by/(?P<username>[-\w]+)/(?P<pk>\d+)/$", views.PostDetail.as_view(), name="single"),
+    re_path(r"delete/(?P<pk>\d+)/$", views.DeletePost.as_view(), name="delete"),
 ]
