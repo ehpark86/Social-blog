22d049770823d2899c8f28d1fed730697ef3a702
Eugene park <ehpark86@gmail.com>
2019-04-26 13:39:28 -0700

    changed path to regex and added humanize to installed apps

--- socialblog/socialblog/urls.py
+++ socialblog/socialblog/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.HomePage.as_view(), name='home'),
-    path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('test/', views.TestPage.as_view(), name='test'),
-    path('thanks/', views.ThanksPage.as_view(), name='thanks')
+    re_path(r"^$", views.HomePage.as_view(), name="home"),
+    re_path(r"^test/$", views.TestPage.as_view(), name="test"),
+    re_path(r"^thanks/$", views.ThanksPage.as_view(), name="thanks"),
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"^accounts/", include("accounts.urls", namespace="accounts")),
+    re_path(r"^accounts/", include("django.contrib.auth.urls")),
+    re_path(r"^posts/", include("posts.urls", namespace="posts")),
+    re_path(r"^groups/", include("groups.urls", namespace="groups")),
 ]
